fc4eea05251debfb6fb37ab844946db14c9a5f89
Daniel Stuart Baxter <shonumi@gmail.com>
2015-01-05 10:36:33 -0600

    Reset RCNT in RegisterRAMReset

--- src/swi.cpp
+++ src/swi.cpp
@@ -332,7 +332,9 @@ void ARM7::swi_registerramreset()
 	}
 
 	//Reset SIO
-	//TODO - Probably only relevant when GBE+ implements multiplayer modes
+	//TODO - See what values SIO MMIO registers have after running BIOS, use them here
+	//For now, just reset RCNT (0x4000134)
+	mem->write_u16(0x4000134, 0x8000);
 
 	//Reset Sound Registers
 	if(reset_flags & 0x40)
