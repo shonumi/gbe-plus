67ec4ff9bd79a59235ab4fcd5893a9d9553e39a5
Daniel Stuart Baxter <shonumi@gmail.com>
2015-01-10 12:44:16 -0600

    Fix a small error in ARM.9 from previous commit

--- src/arm_instr.cpp
+++ src/arm_instr.cpp
@@ -810,7 +810,8 @@ void ARM7::single_data_transfer(u32 current_arm_instruction)
 		{
 			value = get_reg(dest_reg);
 			if(dest_reg == 15) { value += 4; }
-			mem_check_8(base_addr, (value & 0xFF), false);
+			value &= 0xFF;
+			mem_check_8(base_addr, value, false);
 		}
 
 		else
