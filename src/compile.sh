6799be42464ba5a43b8b1b10ada6517d4688ffa5
Daniel Stuart Baxter <shonumi@gmail.com>
2015-05-09 16:28:26 -0500

    Keep GCC objects inside the gba folder.

--- src/compile.sh
+++ src/compile.sh
@@ -1,76 +1,76 @@
 export CPLUS_INCLUDE_PATH=./
 
-if g++ -c -O3 -funroll-loops gba/core.cpp; then
+if g++ -o gba/core.o -c -O3 -funroll-loops gba/core.cpp; then
 	echo -e "Compiling Core...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling Core...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c -O3 -funroll-loops gba/arm7.cpp; then
+if g++ -o gba/arm7.o -c -O3 -funroll-loops gba/arm7.cpp; then
 	echo -e "Compiling ARMv7...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling ARMv7...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c -O3 -funroll-loops gba/dma.cpp; then
+if g++ -o gba/dma.o -c -O3 -funroll-loops gba/dma.cpp; then
 	echo -e "Compiling DMA...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling DMA...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c -O3 -funroll-loops gba/arm_instr.cpp; then
+if g++ -o gba/arm_instr.o -c -O3 -funroll-loops gba/arm_instr.cpp; then
 	echo -e "Compiling ARM instructions...		\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling ARM instructions...		\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c -O3 -funroll-loops gba/thumb_instr.cpp; then
+if g++ -o gba/thumb_instr.o -c -O3 -funroll-loops gba/thumb_instr.cpp; then
 	echo -e "Compiling THUMB instructions...		\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling THUMB instructions...		\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c -O3 -funroll-loops gba/swi.cpp; then
+if g++ -o gba/swi.o -c -O3 -funroll-loops gba/swi.cpp; then
 	echo -e "Compiling SWI...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling SWI...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c -O3 -funroll-loops gba/mmu.cpp; then
+if g++ -o gba/mmu.o -c -O3 -funroll-loops gba/mmu.cpp; then
 	echo -e "Compiling MMU...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling MMU...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c -O3 -funroll-loops gba/lcd.cpp -lSDL; then
+if g++ -o gba/lcd.o -c -O3 -funroll-loops gba/lcd.cpp -lSDL; then
 	echo -e "Compiling LCD...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling LCD...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c -O3 -funroll-loops gba/apu.cpp -lSDL; then
+if g++ -o gba/apu.o -c -O3 -funroll-loops gba/apu.cpp -lSDL; then
 	echo -e "Compiling APU...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling APU...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c -O3 -funroll-loops gba/opengl.cpp -lSDL -lGL; then
+if g++ -o gba/opengl.o -c -O3 -funroll-loops gba/opengl.cpp -lSDL -lGL; then
 	echo -e "Compiling OpenGL...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling OpenGL...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c -O3 -funroll-loops gba/gamepad.cpp -lSDL; then
+if g++ -o gba/gamepad.o -c -O3 -funroll-loops gba/gamepad.cpp -lSDL; then
 	echo -e "Compiling Gamepad...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling Gamepad...			\E[31m[ERROR]\E[37m"
@@ -78,7 +78,7 @@ else
 fi
 
 
-if g++ -c -O3 -funroll-loops gba/config.cpp -lSDL; then
+if g++ -o gba/config.o -c -O3 -funroll-loops gba/config.cpp -lSDL; then
 	echo -e "Compiling Config...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling Config...			\E[31m[ERROR]\E[37m"
@@ -86,14 +86,14 @@ else
 fi
 
 
-if g++ -c -O3 -funroll-loops main.cpp; then
+if g++ -o main.o -c -O3 -funroll-loops main.cpp; then
 	echo -e "Compiling Main...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling Main...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -o gbe_plus core.o arm7.o dma.o arm_instr.o thumb_instr.o swi.o mmu.o gamepad.o lcd.o apu.o opengl.o config.o main.o -lSDL -lGL; then
+if g++ -o gbe_plus gba/core.o gba/arm7.o gba/dma.o gba/arm_instr.o gba/thumb_instr.o gba/swi.o gba/mmu.o gba/gamepad.o gba/lcd.o gba/apu.o gba/opengl.o gba/config.o main.o -lSDL -lGL; then
 	echo -e "Linking Project...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Linking Project...			\E[31m[ERROR]\E[37m"
