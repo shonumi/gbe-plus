8fa1ad2ead7c990be8c6f2e08a2d21292f363f44
Daniel Stuart Baxter <shonumi@gmail.com>
2015-06-02 11:42:13 -0500

    [GB] Simulate bootrom writes via HLE

--- src/dmg/mmu.cpp
+++ src/dmg/mmu.cpp
@@ -667,31 +667,34 @@ bool DMG_MMU::read_file(std::string filename)
 	//Manually HLE MMIO
 	if(!in_bios) 
 	{
-		memory_map[REG_LCDC] = 0x91;
-		memory_map[REG_BGP] = 0xFC;
-		memory_map[REG_OBP0] = 0xFF;
-		memory_map[REG_OBP1] = 0xFF;
-		memory_map[REG_P1] = 0xFF;
-		memory_map[REG_DIV] = 0xAF;
-		memory_map[REG_TAC] = 0xF8;
-		memory_map[0xFF10] = 0x80;
-		memory_map[0xFF11] = 0xBF;
-   		memory_map[0xFF12] = 0xF3; 
-  		memory_map[0xFF14] = 0xBF; 
-   		memory_map[0xFF16] = 0x3F; 
-   		memory_map[0xFF17] = 0x00; 
-   		memory_map[0xFF19] = 0xBF; 
-   		memory_map[0xFF1A] = 0x7F; 
-   		memory_map[0xFF1B] = 0xFF; 
-   		memory_map[0xFF1C] = 0x9F; 
-   		memory_map[0xFF1E] = 0xBF; 
-   		memory_map[0xFF20] = 0xFF; 
-   		memory_map[0xFF21] = 0x00; 
-   		memory_map[0xFF22] = 0x00; 
-   		memory_map[0xFF23] = 0xBF; 
-   		memory_map[0xFF24] = 0x77; 
-   		memory_map[0xFF25] = 0xF3;
-		memory_map[0xFF26] = 0xF1; 
+		write_u8(REG_LCDC, 0x91);
+		write_u8(REG_BGP, 0xFC);
+		write_u8(REG_OBP0, 0xFF);
+		write_u8(REG_OBP1, 0xFF);
+		write_u8(REG_P1, 0xFF);
+		write_u8(REG_DIV, 0xAF);
+		write_u8(REG_TAC, 0xF8);
+		write_u8(0xFF10, 0x80);
+		write_u8(0xFF11, 0xBF);
+   		write_u8(0xFF12, 0xF3); 
+  		write_u8(0xFF14, 0xBF); 
+   		write_u8(0xFF16, 0x3F); 
+   		write_u8(0xFF17, 0x00); 
+   		write_u8(0xFF19, 0xBF); 
+   		write_u8(0xFF1A, 0x7F); 
+   		write_u8(0xFF1B, 0xFF); 
+   		write_u8(0xFF1C, 0x9F); 
+   		write_u8(0xFF1E, 0xBF); 
+   		write_u8(0xFF20, 0xFF); 
+   		write_u8(0xFF21, 0x00); 
+   		write_u8(0xFF22, 0x00); 
+   		write_u8(0xFF23, 0xBF); 
+   		write_u8(0xFF24, 0x77); 
+   		write_u8(0xFF25, 0xF3);
+		write_u8(0xFF26, 0xF1);
+
+		//Some sound registers are set, however, don't actually play sound
+		for(int x = 0; x < 4; x++) { apu_stat->channel[x].playing = false; }
 	}
 
 	//Determine MBC type
