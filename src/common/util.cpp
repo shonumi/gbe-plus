9ecd59cab0cb1f5452930d53ae16d209a339a651
Daniel Stuart Baxter <shonumi@gmail.com>
2015-08-16 16:30:34 -0500

    WIP of PNG saving

--- src/common/util.cpp
+++ src/common/util.cpp
@@ -9,6 +9,8 @@
 // Provides miscellaneous utilities for the emulator
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 #include "util.h"
 
@@ -31,6 +33,140 @@ bool save_png(SDL_Surface* source, std::string filename)
 		std::cout<<"GBE::Error - Source data for " << filename << " is null\n";
 		return false;
 	}
+	
+	//Lock SDL_Surface
+	if(SDL_MUSTLOCK(source)){ SDL_LockSurface(source); }
+	u8* src_pixels = (u8*)source->pixels;
+
+	std::vector<u8> png_bytes;
+
+	//PNG Magic Number - 8 bytes
+	png_bytes.push_back(0x89);
+	png_bytes.push_back(0x50);
+	png_bytes.push_back(0x4E);
+	png_bytes.push_back(0x47);
+	png_bytes.push_back(0x0D);
+	png_bytes.push_back(0x0A);
+	png_bytes.push_back(0x1A);
+	png_bytes.push_back(0x0A);
+
+	//IHDR byte length (always 0x0D for us) - 4 bytes
+	png_bytes.push_back(0x00);
+	png_bytes.push_back(0x00);
+	png_bytes.push_back(0x00);
+	png_bytes.push_back(0x0D);
+
+	//"IHDR" ASCII string - 4 bytes
+	png_bytes.push_back(0x49);
+	png_bytes.push_back(0x48);
+	png_bytes.push_back(0x44);
+	png_bytes.push_back(0x52);
+
+	//Image width - 4 bytes
+	u32 width = source->w;
+
+	for(int x = 24; x >= 0; x-= 8)
+	{
+		png_bytes.push_back((width >> x) & 0xFF);
+	}
+
+	//Image height - 4 bytes
+	u32 height = source->h;
+
+	for(int x = 24; x >= 0; x-= 8)
+	{
+		png_bytes.push_back((height >> x) & 0xFF);
+	}
+
+	//Bit depth (always 0x08 for us) - 1 byte
+	png_bytes.push_back(0x08);
+
+	//Color type (always 0x02 for us) 1 byte
+	png_bytes.push_back(0x02);
+
+	//Compression method (always 0x00 for us, use DEFLATE) - 1 byte
+	//Filter method (always 0x00 for us) - 1 byte
+	//Interlace method (always 0x00 for us) - 1 byte
+	png_bytes.push_back(0x00);
+	png_bytes.push_back(0x00);
+	png_bytes.push_back(0x00);
+
+	//CRC32 of IHDR chunk - 4 bytes
+	u32 ihdr_crc = util::get_crc32((png_bytes.data() + 12), 17);
+
+	for(int x = 24; x >= 0; x-= 8)
+	{
+		png_bytes.push_back((ihdr_crc >> x) & 0xFF);
+	}
+
+	//IDAT byte length - 4 bytes
+	u32 dat_length = (source->w * source->h) * 3;
+
+	for(int x = 24; x >= 0; x-= 8)
+	{
+		png_bytes.push_back((dat_length >> x) & 0xFF);
+	}
+
+	//"IDAT" ASCII string - 4 bytes
+	png_bytes.push_back(0x49);
+	png_bytes.push_back(0x44);
+	png_bytes.push_back(0x41);
+	png_bytes.push_back(0x54);
+
+	//ZLib header - 4 bytes
+	png_bytes.push_back(0x78);
+	png_bytes.push_back(0x00);
+	//TODO Addler32 checksum of data
+
+	//IDAT Chunk header - TODO
+	//Final chunk flag - 1 Byte
+	//Chunk size byte length - 2 Bytes
+	//Chunk size complement - 2 Bytes
+	png_bytes.push_back(0x01);
+
+	//Grab RGB values, then store them
+	for(int x = 0; x < (source->w * source->h) * 4;)
+	{
+		//Skip Alpha values
+		x++;
+
+		png_bytes.push_back(src_pixels[x++]);
+		png_bytes.push_back(src_pixels[x++]);
+		png_bytes.push_back(src_pixels[x++]);
+	}
+
+	//CRC32 of IDAT chunk - 4 bytes
+	u32 idat_crc = util::get_crc32((png_bytes.data() + 37), (dat_length + 4));
+
+	for(int x = 24; x >= 0; x-= 8)
+	{
+		png_bytes.push_back((idat_crc >> x) & 0xFF);
+	}
+
+	//IEND byte length (always 0x00 for us) - 4 bytes
+	png_bytes.push_back(0x00);
+	png_bytes.push_back(0x00);
+	png_bytes.push_back(0x00);
+	png_bytes.push_back(0x00);
+
+	//"IEND" ASCII string
+	png_bytes.push_back(0x49);
+	png_bytes.push_back(0x45);
+	png_bytes.push_back(0x4E);
+	png_bytes.push_back(0x44);
+
+	//CRC32 of IEND - Precalculated
+	png_bytes.push_back(0xAE);
+	png_bytes.push_back(0x42);
+	png_bytes.push_back(0x60);
+	png_bytes.push_back(0x82);
+
+	std::ofstream file(filename.c_str(), std::ios::binary);
+	file.write(reinterpret_cast<char*> (&png_bytes[0]), png_bytes.size());
+	file.close();
+
+	//Unlock SDL_Surface
+	if(SDL_MUSTLOCK(source)) { SDL_UnlockSurface(source); }
 
 	return true;
 }
