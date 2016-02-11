24afc2ccdfead529a85acfc857d134f1fe0d0e29
Daniel Stuart Baxter <shonumi@gmail.com>
2016-02-10 22:11:20 -0600

    [DMG] Crude version of DMG-OBJ scaling for DMG on GBA stretching

--- src/dmg/lcd.cpp
+++ src/dmg/lcd.cpp
@@ -1051,11 +1051,16 @@ void DMG_LCD::render_dmg_obj_scanline()
 					//Apply DMG/GBC on GBA stretching if applicable
 					if((config::resize_mode == 2) && (last_scanline_pixel < 160))
 					{
-						u8 flipped_y = (obj[sprite_id].h_flip) ? lcd_stat.flip_8[y] : y;
-						u8 stretched_pos = (flipped_y & 0x1) ? (((last_scanline_pixel + 1) >> 1) * 3) - 1 : ((last_scanline_pixel >> 1) * 3);
-
+						u8 stretched_pos = (lcd_stat.flip_8[y] & 0x1) ? (((last_scanline_pixel + 1) >> 1) * 3) - 1 : ((last_scanline_pixel >> 1) * 3);
 						stretched_buffer[stretched_pos] = scanline_buffer[last_scanline_pixel];
-						if((flipped_y & 0x1) == 0) { stretched_pos++; stretched_buffer[stretched_pos] = scanline_buffer[last_scanline_pixel]; }
+
+						if(lcd_stat.flip_8[y] & 0x1)
+						{
+							u8 old_pos = stretched_pos - 1;
+							u8 blend_pos = old_pos - 1;
+							stretched_buffer[old_pos] = scanline_buffer[last_scanline_pixel];
+							stretched_buffer[old_pos] = util::rgb_blend(stretched_buffer[old_pos], stretched_buffer[blend_pos]);
+						}
 					}
 
 					//Render HD
