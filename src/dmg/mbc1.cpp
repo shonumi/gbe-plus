58268570a3064fbe0f16ff6c9ff423e949576acb
Daniel Stuart Baxter <shonumi@gmail.com>
2016-04-22 22:27:49 -0500

    [DMG] Added cart RAM handling for MBC1M

--- src/dmg/mbc1.cpp
+++ src/dmg/mbc1.cpp
@@ -22,7 +22,7 @@ void DMG_MMU::mbc1_write(u16 address, u8 value)
 	}
 
 	//MBC register - Enable or Disable RAM Banking
-	if(address <= 0x1FFF)
+	if((address <= 0x1FFF) && (cart.ram))
 	{
 		if((value & 0xF) == 0xA) { ram_banking_enabled = true; }
 		else { ram_banking_enabled = false; }
@@ -81,18 +81,31 @@ u8 DMG_MMU::mbc1_read(u16 address)
 /****** Performs write operations specific to the MBC1M ******/
 void DMG_MMU::mbc1_multicart_write(u16 address, u8 value)
 {
-	//MBC register - Select ROM bank - Bits 0 to 4
+	//Write to External RAM
+	if((address >= 0xA000) && (address <= 0xBFFF) && (cart.ram))
+	{
+		//MBC1M only accesses RAM Bank 0
+		random_access_bank[0][address - 0xA000] = value;
+	}
+
+	//MBC register - Enable or Disable RAM
+	if((address <= 0x1FFF) && (cart.ram))
+	{
+		if((value & 0xF) == 0xA) { ram_banking_enabled = true; }
+		else { ram_banking_enabled = false; }
+	}
+
+	//MBC register - Select ROM bank - Bits 0 to 3
 	if((address >= 0x2000) && (address <= 0x3FFF)) 
 	{ 
 		rom_bank = (value & 0xF);
 	}
 
-	//MBC register - Select ROM bank bits 5 to 6 or Set or RAM bank
+	//MBC register - Select ROM uppper 2 bank bits
 	else if((address >= 0x4000) && (address <= 0x5FFF)) { bank_bits = (value & 0x3); }
 
 	//MBC register - ROM/RAM Select
-	//For MBC1 multicarts, this selects whether or not to move another bank into ROM0
-	//For multicarts, this also converts banks when reading from ROM1 (i.e. calculating which bank to use is different)
+	//For the MBC1M, this selects whether or not to move another bank into ROM0
 	else if((address >= 0x6000) && (address <= 0x7FFF)) { bank_mode = (value & 0x1); }
 }
 
@@ -137,5 +150,13 @@ u8 DMG_MMU::mbc1_multicart_read(u16 address)
 
 		//When reading from Banks 0-1, just use the memory map
 		else { return memory_map[address]; }
+	}
+
+	//Read cartridge RAM
+	else if((address >= 0xA000) && (address <= 0xBFFF))
+	{
+		//MBC1M only accesses RAM Bank 0
+		if(ram_banking_enabled) { return random_access_bank[0][address - 0xA000]; }
+		else { return 0x00; }
 	}
 }
