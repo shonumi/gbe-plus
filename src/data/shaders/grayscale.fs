e056332a59346b905ffee348827997dd98b4f7c6
Daniel Stuart Baxter <shonumi@gmail.com>
2016-08-14 11:04:27 -0500

    [OGL] Added grayscale fragment shader

---
+++ src/data/shaders/grayscale.fs
@@ -0,0 +1,28 @@
+// GB Enhanced+ Copyright Daniel Baxter 2016
+// Licensed under the GPLv2
+// See LICENSE.txt for full license text
+
+// File : grayscale.fs
+// Date : August 14, 2016
+// Description : GBE+ Fragment Shader - Grayscale
+//
+// Makes everything look black and white/shades of gray
+
+#version 330 core
+in vec2 texture_coordinates;
+
+out vec4 color;
+
+uniform sampler2D screen_texture;
+
+void main()
+{
+	vec4 input_color = texture(screen_texture, texture_coordinates);
+	vec4 output_color = vec4(0, 0, 0, 0);
+
+	output_color.r = (input_color.r * 0.21) + (input_color.g * 0.72) + (input_color.b * 0.07);
+	output_color.g = output_color.r;
+	output_color.b = output_color.r;
+
+	color = output_color;
+}
