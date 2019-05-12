bd045eb09e7debd12a56cd5536d86719e5f8f159
Daniel Stuart Baxter <shonumi@gmail.com>
2019-05-11 22:36:15 -0500

    [Qt] Added Singer IZEK 1500 GUI option for SIO devices

--- src/qt/general_settings.cpp
+++ src/qt/general_settings.cpp
@@ -165,6 +165,7 @@ gen_settings::gen_settings(QWidget *parent) : QDialog(parent)
 	sio_dev->addItem("Progress Chip Gate");
 	sio_dev->addItem("Beast Link Gate");
 	sio_dev->addItem("Power Antenna");
+	sio_dev->addItem("Singer IZEK 1500");
 
 	QHBoxLayout* sio_layout = new QHBoxLayout;
 	sio_layout->setAlignment(Qt::AlignTop | Qt::AlignLeft);
