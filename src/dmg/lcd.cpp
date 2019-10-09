d63b8ecc9973033bc45358dc92e875322259d0df
Daniel Stuart Baxter <shonumi@gmail.com>
2019-10-08 20:32:40 -0500

    Fix segfaults for new OpenGL initialization method

--- src/dmg/lcd.cpp
+++ src/dmg/lcd.cpp
@@ -218,10 +218,13 @@ bool DMG_LCD::init()
 		}
 
 		//Setup OpenGL rendering
-		if(config::use_opengl && !opengl_init())
+		if(config::use_opengl)
 		{
-			std::cout<<"LCD::Error - Could not initialize OpenGL\n";
-			return false;
+			if(!opengl_init())
+			{
+				std::cout<<"LCD::Error - Could not initialize OpenGL\n";
+				return false;
+			}
 		}
 
 		//Set up software rendering
--- src/nds/lcd.cpp
+++ src/nds/lcd.cpp
@@ -334,10 +334,13 @@ bool NTR_LCD::init()
 		}
 
 		//Setup OpenGL rendering
-		if(config::use_opengl && !opengl_init())
+		if(config::use_opengl)
 		{
-			std::cout<<"LCD::Error - Could not initialize OpenGL\n";
-			return false;
+			if(!opengl_init())
+			{
+				std::cout<<"LCD::Error - Could not initialize OpenGL\n";
+				return false;
+			}
 		}
 
 		//Set up software rendering
