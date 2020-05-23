3e7c2c74cda37ac860e23fb4ed4c235eb0f32a0b
Daniel Stuart Baxter <shonumi@gmail.com>
2020-05-23 13:17:40 -0500

    [DMG] Fix remaining cursor issues for sewing subscreen

--- src/dmg/sio.cpp
+++ src/dmg/sio.cpp
@@ -1728,12 +1728,7 @@ void DMG_SIO::singer_izek_data_process()
 
 				if(singer_izek.device_mode == 0)
 				{
-					singer_izek.last_x = singer_izek.current_x;
-					singer_izek.last_y = 0;
-					singer_izek.current_y = 0;
-
-					singer_izek.x_offset = (singer_izek.cam_x + singer_izek.start_x) - singer_izek.current_x;
-					singer_izek.y_offset = singer_izek.cam_y;
+					singer_izek.current_x = singer_izek.start_x;
 				}
 
 				else if(singer_izek.device_mode == 1)
@@ -2152,6 +2147,7 @@ void DMG_SIO::singer_izek_update()
 		{
 			if(singer_izek.device_mode == 0)
 			{
+				std::cout<<"UPDATE MATE\n";
 				singer_izek.x_offset = (singer_izek.cam_x + singer_izek.start_x) - singer_izek.current_x;
 				singer_izek.y_offset = singer_izek.cam_y;
 			}
