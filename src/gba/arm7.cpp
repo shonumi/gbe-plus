dcaa0aa6a67f33f76d7474629287aff9ae0ebe31
Daniel Stuart Baxter <shonumi@gmail.com>
2020-07-14 23:49:46 -0500

    [GBA] Fix ARM RSC instruction. Fix LSL and LSR carry out when shifting greater than 32

--- src/gba/arm7.cpp
+++ src/gba/arm7.cpp
@@ -987,7 +987,12 @@ u8 ARM7::logical_shift_left(u32& input, u8 offset)
 		u32 carry_test = input << (offset - 1);
 		carry_out = (carry_test & 0x80000000) ? 1 : 0;
 
-		if(offset >= 32) { input = 0; }
+		if(offset >= 32)
+		{
+			input = 0;
+			carry_out = 0;
+		}
+
 		else { input <<= offset; }
 	}
 
@@ -1010,7 +1015,12 @@ u8 ARM7::logical_shift_right(u32& input, u8 offset)
 		u32 carry_test = input >> (offset - 1);
 		carry_out = (carry_test & 0x1) ? 1 : 0;
 
-		if(offset >= 32) { input = 0; }
+		if(offset >= 32)
+		{
+			input = 0;
+			carry_out = 0;
+		}
+
 		else { input >>= offset; }
 	}
 
--- src/gba/arm_instr.cpp
+++ src/gba/arm_instr.cpp
@@ -316,7 +316,7 @@ void ARM7::data_processing(u32 current_arm_instruction)
 			set_reg(dest_reg, result);
 
 			//Update condtion codes
-			if(set_condition) { update_condition_arithmetic((operand + shift_out - 1), input, result, false); }
+			if(set_condition) { update_condition_arithmetic(operand, (input + shift_out - 1), result, false); }
 			break;
 
 		//TST
