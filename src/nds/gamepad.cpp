6329f2e47f71358882ff63194c928e01b08fa5b7
Daniel Stuart Baxter <shonumi@gmail.com>
2021-03-12 23:27:31 -0600

    [NDS] More work on Virtual Cursor updates via NDS GamePad

--- src/nds/gamepad.cpp
+++ src/nds/gamepad.cpp
@@ -776,6 +776,102 @@ void NTR_GamePad::process_joystick(int pad, bool pressed)
 	
 	//Misc Context Key 2 release
 	else if((pad == config::con_joy_2) && (!pressed)) { con_flags &= ~0x200; }
+
+	//Misc Context Left press
+	else if((pad == config::con_joy_left) && (pressed))
+	{
+		//Adjust Virtual Cursor X coordinate
+		vc_delta_x = -8;
+
+		con_flags |= 0x1;
+		con_flags |= 0x10;
+		con_flags &= ~0x2;
+	}
+
+	//Context Left release
+	else if((pad == config::con_joy_left) && (!pressed))
+	{
+		//Adjust Virtual Cursor X coordinate
+		vc_delta_x = 0;
+
+		con_flags &= ~0x1;
+		con_flags &= ~0x10;
+
+		if(con_flags & 0x20) { con_flags |= 0x2; }
+		else { con_flags &= ~0x2; }
+	}
+
+	//Context Right press
+	else if((pad == config::con_joy_right) && (pressed))
+	{
+		//Adjust Virtual Cursor X coordinate
+		vc_delta_x = 8;
+
+		con_flags |= 0x2;
+		con_flags |= 0x20;
+		con_flags &= ~0x1;
+	}
+
+	//Context Right release
+	else if((pad == config::con_joy_right) && (!pressed))
+	{
+		//Adjust Virtual Cursor X coordinate
+		vc_delta_x = 0;
+
+		con_flags &= ~0x2;
+		con_flags &= ~0x20;
+
+		if(con_flags & 0x10) { con_flags |= 0x1; }
+		else { con_flags &= ~0x1; }
+	}
+
+	//Context Up press
+	else if((pad == config::con_joy_up) && (pressed))
+	{
+		//Adjust Virtual Cursor Y coordinate
+		vc_delta_y = -8;
+
+		con_flags |= 0x4;
+		con_flags |= 0x40;
+		con_flags &= ~0x8;
+	}
+
+	//Context Up release
+	else if((pad == config::con_joy_up) && (!pressed))
+	{
+		//Adjust Virtual Cursor Y coordinate
+		vc_delta_y = 0;
+
+		con_flags &= ~0x4;
+		con_flags &= ~0x40;
+
+		if(con_flags & 0x80) { con_flags |= 0x8; }
+		else { con_flags &= ~0x8; }
+	}
+
+	//Context Down press
+	else if((pad == config::con_joy_down) && (pressed))
+	{
+		//Adjust Virtual Cursor Y coordinate
+		vc_delta_y = 8;
+
+		con_flags |= 0x8;
+		con_flags |= 0x80;
+		con_flags &= ~0x4;
+	}
+
+	//Context Down release
+	else if((pad == config::con_joy_down) && (!pressed))
+	{
+		//Adjust Virtual Cursor Y coordinate
+		vc_delta_y = 0;
+
+		con_flags &= ~0x8;
+		con_flags &= ~0x80;
+
+		if(con_flags & 0x40) { con_flags |= 0x4; }
+		else { con_flags &= ~0x4; }
+	}
 }
 
 /****** Processes input based on the mouse ******/
@@ -850,6 +946,9 @@ void NTR_GamePad::process_virtual_cursor()
 		vc_counter = 0;
 		vc_x += vc_delta_x;
 		vc_y += vc_delta_y;
+
+		if(vc_x >= 252) { vc_x = 252; }
+		if(vc_y >= 188) { vc_y = 188; }
 	}
 }
 		
