70b5349750f966f572215b7abbfde886654dbb5e
Daniel Stuart Baxter <shonumi@gmail.com>
2021-07-17 23:55:59 -0500

    [MIN] Added CLI debugging option to break on next VBlank

--- src/min/debug.cpp
+++ src/min/debug.cpp
@@ -30,8 +30,25 @@ void MIN_core::debug_step()
 	//Use CLI for all debugging
 	bool printed = false;
 
+	//When running until next VBlank, stop when done
+	if((db_unit.vb_count) || (db_unit.last_command == "vb"))
+	{
+		if((db_unit.vb_count == 2) && (core_cpu.controllers.video.lcd_stat.prc_counter > 0x01)) { db_unit.vb_count--; }
+
+		if((db_unit.vb_count == 1) && (core_cpu.controllers.video.lcd_stat.prc_counter == 0x01))
+		{
+			db_unit.vb_count = 0;
+			db_unit.last_mnemonic = debug_get_mnemonic(core_cpu.reg.pc);
+			db_unit.last_command = "n";
+
+			debug_display();
+			debug_process_command();
+			printed = true;
+		}
+	}
+
 	//In continue mode, if breakpoints exist, try to stop on one
-	if((db_unit.breakpoints.size() > 0) && (db_unit.last_command == "c"))
+	else if((db_unit.breakpoints.size() > 0) && (db_unit.last_command == "c"))
 	{
 		for(int x = 0; x < db_unit.breakpoints.size(); x++)
 		{
@@ -1470,6 +1487,15 @@ void MIN_core::debug_process_command()
 				valid_command = true;
 				db_unit.last_command = "ri";
 			}
+		}
+
+		//Run emulation until next system VBlank
+		else if(command == "vb")
+		{
+			db_unit.vb_count = 2;
+			
+			valid_command = true;
+			db_unit.last_command = "vb";
 		}
 
 		//Print all instructions to the screen
