8ece1cf6ea3b705cf3f1d011140ffb6fef152a7c
Daniel Stuart Baxter <shonumi@gmail.com>
2022-03-15 12:18:05 -0500

    [Qt] Added Qt GUI option for GBA Music Recorder/Jukebox

--- src/qt/main_menu.cpp
+++ src/qt/main_menu.cpp
@@ -702,7 +702,8 @@ void main_menu::boot_game()
 		case 0xA: config::cart_type = AGB_TILT_SENSOR; break;
 		case 0xB: config::cart_type = AGB_8M_DACS; break;
 		case 0xC: config::cart_type = AGB_AM3; break;
-		case 0xD: config::cart_type = NDS_IR_CART; break;
+		case 0xD: config::cart_type = AGB_JUKEBOX; break;
+		case 0xE: config::cart_type = NDS_IR_CART; break;
 	}
 
 	//Check rumble status
