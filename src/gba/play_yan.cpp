fce51d4839ccd84b61ee6b61dce58c7ffad4cb5d
Daniel Stuart Baxter <shonumi@gmail.com>
2022-10-28 19:19:13 -0500

    [GBA] Use length of video files specified in GBE+'s video.txt file for playback length on Play-Yan

--- src/gba/play_yan.cpp
+++ src/gba/play_yan.cpp
@@ -327,7 +327,6 @@ void AGB_MMU::write_play_yan(u32 address, u8 value)
 				play_yan.irq_count = 0;
 				play_yan.video_data_addr = 0;
 				play_yan.video_progress = 0;
-				play_yan.video_length = (15 * 0x20 * 30);
 				play_yan.is_video_playing = true;
 
 				play_yan.capture_command_stream = true;
@@ -430,6 +429,7 @@ void AGB_MMU::write_play_yan(u32 address, u8 value)
 					if(temp_str == play_yan.video_files[x])
 					{
 						play_yan.video_file_index = x;
+						play_yan.video_length = (play_yan.video_times[x] * 0x20 * 30);
 						break;
 					}
 				}
@@ -641,7 +641,7 @@ void AGB_MMU::process_play_yan_irq()
 					play_yan.irq_data_ptr = play_yan.video_play_data[0];
 					play_yan.irq_count = 1;
 					play_yan.irq_len = 2;
-					play_yan.irq_repeat = 10000;
+					play_yan.irq_repeat = (play_yan.video_length / 0x20);
 				}
 
 				//Repeat thumbnail IRQs as necessary
