7c0655e780b2f4e8f98caa9c746d2fadf544976d
Daniel Stuart Baxter <shonumi@gmail.com>
2025-11-03 14:29:32 -0600

    [DMG] Remove  DMG core's deprecated patching functions

--- src/dmg/mmu.h
+++ src/dmg/mmu.h
@@ -233,9 +233,6 @@ class DMG_MMU
 	bool save_backup(std::string filename);
 	bool load_backup(std::string filename);
 
-	bool patch_ips(std::string filename);
-	bool patch_ups(std::string filename);
-
 	//Memory Bank Controller dedicated read/write operations
 	void mbc_write(u16 address, u8 value);
 	u8 mbc_read(u16 address);
