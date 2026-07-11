67989fc4393aaf6ff86445774bd1b9e01e9b2710
Daniel Stuart Baxter <shonumi@gmail.com>
2026-07-11 14:39:11 -0500

    [NDS] Loop ADPCM data properly

--- src/nds/apu.cpp
+++ src/nds/apu.cpp
@@ -227,7 +227,7 @@ void NTR_APU::generate_channel_samples(s32* stream, int length, u8 id)
 					//Loop sound
 					if(loop_mode == 1)
 					{
-						apu_stat.channel[id].data_pos = apu_stat.channel[id].data_src;
+						apu_stat.channel[id].adpcm_pos = (apu_stat.channel[id].loop_start * 8);
 						apu_stat.channel[id].samples = ((apu_stat.channel[id].length - 1) * 8);
 					}
 
